5fe0f8767ea50a35c56534ff3ee4396c08d3ed8e
eugene-bakulin <bakulin.eugene@gmail.com>
2022-08-22 17:54:39 +0400

    feat: add scroll to header after clicking on article reference

--- corejs-dev/src/components/Theory/TheoryPage.tsx
+++ corejs-dev/src/components/Theory/TheoryPage.tsx
@@ -37,64 +37,64 @@ const TheoryPage: React.FC = () => {
             <div className="articles-nav-wrap">
                 <Nav className="articles-nav flex-column">
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art1')}} className={`article-nav-link ${activePage === 'art1'?'active':''}`}>Типы данных</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art1'); scrollUp()}} className={`article-nav-link ${activePage === 'art1'?'active':''}`}>Типы данных</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art2')}} className={`article-nav-link ${activePage === 'art2'?'active':''}`}>Числа Number</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art2'); scrollUp()}} className={`article-nav-link ${activePage === 'art2'?'active':''}`}>Числа Number</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art3')}} className={`article-nav-link ${activePage === 'art3'?'active':''}`}>Строки String</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art3'); scrollUp()}} className={`article-nav-link ${activePage === 'art3'?'active':''}`}>Строки String</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art4')}} className={`article-nav-link ${activePage === 'art4'?'active':''}`}>Переменные</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art4'); scrollUp()}} className={`article-nav-link ${activePage === 'art4'?'active':''}`}>Переменные</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art5')}} className={`article-nav-link ${activePage === 'art5'?'active':''}`}>Let, const, var</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art5'); scrollUp()}} className={`article-nav-link ${activePage === 'art5'?'active':''}`}>Let, const, var</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art6')}} className={`article-nav-link ${activePage === 'art6'?'active':''}`}>Условное ветвление: if, '?'</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art6'); scrollUp()}} className={`article-nav-link ${activePage === 'art6'?'active':''}`}>Условное ветвление: if, '?'</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art7')}} className={`article-nav-link ${activePage === 'art7'?'active':''}`}>Конструкция switch</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art7'); scrollUp()}} className={`article-nav-link ${activePage === 'art7'?'active':''}`}>Конструкция switch</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art8')}} className={`article-nav-link ${activePage === 'art8'?'active':''}`}>Преобразование типов</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art8'); scrollUp()}} className={`article-nav-link ${activePage === 'art8'?'active':''}`}>Преобразование типов</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art9')}} className={`article-nav-link ${activePage === 'art9'?'active':''}`}>Операторы сравнения</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art9'); scrollUp()}} className={`article-nav-link ${activePage === 'art9'?'active':''}`}>Операторы сравнения</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art10')}} className={`article-nav-link ${activePage === 'art10'?'active':''}`}>Полифилы</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art10'); scrollUp()}} className={`article-nav-link ${activePage === 'art10'?'active':''}`}>Полифилы</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art11')}} className={`article-nav-link ${activePage === 'art11'?'active':''}`}>Функции</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art11'); scrollUp()}} className={`article-nav-link ${activePage === 'art11'?'active':''}`}>Функции</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art12')}} className={`article-nav-link ${activePage === 'art12'?'active':''}`}>Function Expressions</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art12'); scrollUp()}} className={`article-nav-link ${activePage === 'art12'?'active':''}`}>Function Expressions</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art13')}} className={`article-nav-link ${activePage === 'art13'?'active':''}`}>Стрелочные функции</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art13'); scrollUp()}} className={`article-nav-link ${activePage === 'art13'?'active':''}`}>Стрелочные функции</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art14')}} className={`article-nav-link ${activePage === 'art14'?'active':''}`}>Дата и время</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art14'); scrollUp()}} className={`article-nav-link ${activePage === 'art14'?'active':''}`}>Дата и время</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art15')}} className={`article-nav-link ${activePage === 'art15'?'active':''}`}>Объекты. Основы</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art15'); scrollUp()}} className={`article-nav-link ${activePage === 'art15'?'active':''}`}>Объекты. Основы</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art16')}} className={`article-nav-link ${activePage === 'art16'?'active':''}`}>Объекты. Копирование</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art16'); scrollUp()}} className={`article-nav-link ${activePage === 'art16'?'active':''}`}>Объекты. Копирование</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art17')}} className={`article-nav-link ${activePage === 'art17'?'active':''}`}>Методы объектов</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art17'); scrollUp()}} className={`article-nav-link ${activePage === 'art17'?'active':''}`}>Методы объектов</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art18')}} className={`article-nav-link ${activePage === 'art18'?'active':''}`}>Массивы</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art18'); scrollUp()}} className={`article-nav-link ${activePage === 'art18'?'active':''}`}>Массивы</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art19')}} className={`article-nav-link ${activePage === 'art19'?'active':''}`}>Методы массивов</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art19'); scrollUp()}} className={`article-nav-link ${activePage === 'art19'?'active':''}`}>Методы массивов</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link to="" onClick={()=>{setActivatePage('art20')}} className={`article-nav-link ${activePage === 'art20'?'active':''}`}>Циклы</Link>
+                        <Link to="" onClick={()=>{setActivatePage('art20'); scrollUp()}} className={`article-nav-link ${activePage === 'art20'?'active':''}`}>Циклы</Link>
                     </Nav.Item>
                 </Nav>
             </div>
