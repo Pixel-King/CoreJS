cdc68afa52dee7a1712579d0221de75b711d0dba
Yaraslau Borodavkin <y.borodavkin2011@gmail.com>
2022-09-01 22:01:55 +0300

    feat: add profile

---
+++ corejs-dev/src/components/Profile/Profile.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = () => {
+    return (
+        <>
+            <div className='user-profile__wrap'>
+                <section className='user-inf__wrap'>
+
+                </section>
+                <section className='user-stat__wrap'>
+                    
+                </section>
+            </div>
+        </>
+    );
+}
+
+export default Profile;
