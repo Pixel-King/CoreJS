408943692b29929adbc0e1dad86007e895f04509
Yaraslau Borodavkin <y.borodavkin2011@gmail.com>
2022-09-01 21:56:28 +0300

    feat: add provider to app

--- corejs-dev/src/index.tsx
+++ corejs-dev/src/index.tsx
@@ -1,18 +1,21 @@
 import  * as React from 'react';
-import { render } from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client';
-import { ThemeProvider } from 'react-bootstrap';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>
+
 );
 
