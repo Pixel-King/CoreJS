1e311a2b70c55573c73d41c5ecb653bc982e3a60
Yaraslau Borodavkin <y.borodavkin2011@gmail.com>
2022-09-01 21:57:39 +0300

    feat: add state redux for auth

---
+++ corejs-dev/src/components/Autorisation/SignInForm/authSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { RootState } from '../../../app/store';
+
+interface authState {
+    auth: boolean,
+}
+  
+const initialState: authState = {
+    auth: false
+}
+
+export const authSlice = createSlice({
+    name: 'userAuth',
+    initialState,
+    reducers: {
+        initAuth: (state) => {
+            state.auth = true;
+        },
+        removeAuth: (state) => {
+            state.auth = false;
+        }
+    }
+});
+
+export const { initAuth, removeAuth } = authSlice.actions
+
+export const selectAuth = (state: RootState) => state.userAuth.auth
+
+export default authSlice.reducer
