0b0e9a83342326dce78fb884c9483d81ee37c5f8
Yaraslau Borodavkin <y.borodavkin2011@gmail.com>
2022-09-02 20:50:10 +0300

    feat: add valid reg form

--- corejs-dev/src/components/Autorisation/SignInForm/SignInForm.tsx
+++ corejs-dev/src/components/Autorisation/SignInForm/SignInForm.tsx
@@ -9,7 +9,7 @@ import { setState } from "../../User/userSlice";
 import validator from "validator";
 import ShowPas from "./ShowPasSVG/ShowPas";
 
-const SignInForm:React.FC = () =>{
+const SignInForm: React.FC = () => {
   const [modalShow, setModalShow] = React.useState(false);
 
   const [email, setEmail] = React.useState<string>('');
@@ -27,11 +27,11 @@ const SignInForm:React.FC = () =>{
   const dispatch = useAppDispatch();
   const target = React.useRef(null);
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     ValidEmail();
   }, [email]);
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     validPassword()
   }, [password]);
 
@@ -42,6 +42,7 @@ const SignInForm:React.FC = () =>{
       setPasswordMesValid('Введите пароль 8-20 символов и без пробелов');
     }
   }
+
   function ValidEmail() {
     if (validator.isEmail(email)) {
       setEmailMesValid('');
@@ -52,7 +53,7 @@ const SignInForm:React.FC = () =>{
 
   async function authAsync() {
     try {
-      const res = await axios.post('http://localhost:4200/auth/login', {email, password});
+      const res = await axios.post('http://localhost:4200/auth/login', { email, password });
       if (res.status === 201) {
         dispatch(initAuth());
         setModalShow(true);
@@ -74,40 +75,40 @@ const SignInForm:React.FC = () =>{
 
   return (
     <>
-    {!auth && <div className="auth-wrap" >
-      <form className="auth-form">
-        <div className="auth-email" ref={target}>
-          <label htmlFor="auth-email" className='m-2 fs-4'>Адрес эл. почты:</label>
-          <input 
-            type="email" 
-            name="auth-email" 
-            id="auth-email" 
-            className={emailMesValid ? "wrong-input mb-4 fs-5" : "success-input mb-4 fs-5"}
-            onChange={(e)=>setEmail(e.target.value)} 
-          />
-          <span className="auth-err">{emailMesValid}</span>
-        </div>
-        <div 
-          className="auth-pas"
-          onMouseEnter={()=> setShowButPas(true)}
-          onMouseLeave={() => setShowButPas(false)}
-        >
-          <label htmlFor="auth-pas" className='m-2 fs-4'>Пароль:</label>
-          <input 
-            type={showPas ? "text" : "password"} 
-            name="auth-pas" 
-            id="auth-pas"
-            className={passwordMesValid ? "wrong-input mb-4 fs-5" : "success-input mb-4 fs-5"}
-            onChange={(e)=>setPassword(e.target.value)}
-          />
-          <span className="auth-err">{passwordMesValid}</span>
-          {showButPas && 
-          <div className="show-pas-img" onClick={()=>{setShowPas(!showPas)}}>
-            <ShowPas></ShowPas>
+      {!auth && <div className="auth-wrap" >
+        <form className="auth-form">
+          <div className="auth-email" ref={target}>
+            <label htmlFor="auth-email" className='m-2 fs-4'>Адрес эл. почты:</label>
+            <input
+              type="email"
+              name="auth-email"
+              id="auth-email"
+              className={emailMesValid ? "wrong-input mb-4 fs-5" : "success-input mb-4 fs-5"}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <span className="auth-err">{emailMesValid}</span>
           </div>
-          }
-        </div>
-        {/* <input 
+          <div
+            className="auth-pas"
+            onMouseEnter={() => setShowButPas(true)}
+            onMouseLeave={() => setShowButPas(false)}
+          >
+            <label htmlFor="auth-pas" className='m-2 fs-4'>Пароль:</label>
+            <input
+              type={showPas ? "text" : "password"}
+              name="auth-pas"
+              id="auth-pas"
+              className={passwordMesValid ? "wrong-input mb-4 fs-5" : "success-input mb-4 fs-5"}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <span className="auth-err">{passwordMesValid}</span>
+            {showButPas &&
+              <div className="show-pas-img" onClick={() => { setShowPas(!showPas) }}>
+                <ShowPas></ShowPas>
+              </div>
+            }
+          </div>
+          {/* <input 
           type="checkbox" 
           name="Show Password" 
           id="checked-pas__auth"
@@ -115,57 +116,57 @@ const SignInForm:React.FC = () =>{
           onChange={()=>setChecked(!checked)}
         /> 
         Показать пароль */}
-        <button 
-          className="mt-4 btn btn-primary"
+          <button
+            className="mt-4 btn btn-primary"
 
-          disabled={!emailMesValid && !passwordMesValid ? false : true}
-          onClick={(e)=>{
-            e.preventDefault();
-            authAsync();
-        }}>Подтвердить</button>
-        <Overlay target={target.current} show={error ? true : false} placement="top">
-        {({ placement, arrowProps, show: _show, popper, ...props }) => (
-          <div
-            {...props}
-            style={{
-              position: 'absolute',
-              backgroundColor: 'rgba(255, 100, 100, 0.85)',
-              padding: '2px 10px',
-              color: 'white',
-              borderRadius: 3,
-              ...props.style,
-            }}
-          >
-            Не правильный адрес эл. почты или пароль!
-          </div>
-        )}
-      </Overlay>
-      </form>
-    </div>}
-    <Modal
-      show={modalShow}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      onHide={() => hide()}
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter" className="ta-center">
-          Успех!
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p>
-            Поздравляю! Вы успешно авторизировались на сайте. 
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={()=>hide()}>
-          <Link to='/' className="nav-link">Продолжить</Link>
-        </Button>
-      </Modal.Footer>
-    </Modal>
-    </> 
+            disabled={!emailMesValid && !passwordMesValid ? false : true}
+            onClick={(e) => {
+              e.preventDefault();
+              authAsync();
+            }}>Подтвердить</button>
+          <Overlay target={target.current} show={error ? true : false} placement="top">
+            {({ placement, arrowProps, show: _show, popper, ...props }) => (
+              <div
+                {...props}
+                style={{
+                  position: 'absolute',
+                  backgroundColor: 'rgba(255, 100, 100, 0.85)',
+                  padding: '2px 10px',
+                  color: 'white',
+                  borderRadius: 3,
+                  ...props.style,
+                }}
+              >
+                Не правильный адрес эл. почты или пароль!
+              </div>
+            )}
+          </Overlay>
+        </form>
+      </div>}
+      <Modal
+        show={modalShow}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        onHide={() => hide()}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter" className="ta-center">
+            Успех!
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Поздравляю! Вы успешно авторизировались на сайте.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => hide()}>
+            <Link to='/' className="nav-link">Продолжить</Link>
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 export default SignInForm;
