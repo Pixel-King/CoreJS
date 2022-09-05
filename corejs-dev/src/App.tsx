363836f5be43dff226de25d695db2b6f65197eab
Yaraslau Borodavkin <y.borodavkin2011@gmail.com>
2022-09-05 05:30:02 +0300

    feat: change self profile and add pafe for change profile

--- corejs-dev/src/App.tsx
+++ corejs-dev/src/App.tsx
@@ -19,6 +19,7 @@ import { initAuth, selectAuth } from './components/Autorisation/SignInForm/authS
 import User from './components/User/User';
 import Profile from './components/Profile/Profile';
 import { setState } from './components/User/userSlice';
+import ChangeProfile from './components/ChangeProfile/ChangeProfile';
 
 const App: React.FC = () => {
   const auth = useAppSelector(selectAuth);
@@ -99,6 +100,7 @@ const App: React.FC = () => {
           <Route path='testspractice' element={ <TestsRender type='practice'/> } />
           <Route path='testsmix' element={ <TestsRender type='mix'/> } />
           { auth && <Route path='my-profile' element={ <Profile/> } />}
+          { auth && <Route path='change' element={ <ChangeProfile/> } />}
         </Routes>
       </main>
       <footer className='footer'>
---
+++ corejs-dev/src/components/ChangeProfile/ChangeForm/ResetProgresForm.tsx
@@ -0,0 +1,41 @@
+import axios, { AxiosError } from 'axios';
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useAppSelector } from '../../../app/hooks';
+import { selectUserState } from '../../User/userSlice';
+
+const ResetProgresForm: React.FC = () =>{
+    const user = useAppSelector(selectUserState);
+
+    async function resetProgreAsync() {
+        try {
+            let config = {
+                headers: {
+                  Authorization: `Bearer ${user.token}`
+                }
+              }
+
+            const res = await axios.post(`http://localhost:4200/users/resetprogress/${user.id}`, config);
+        } catch (e: unknown) {
+            const err = e as AxiosError;
+            console.error(err);
+
+        }
+    }
+
+    return (
+        <>
+            <form className='simple_form edit_user'>
+                <section>
+                    <div className='inf-header reset-progres'>
+                        <h4>Прогресс</h4>
+                        {' '}
+                        <Button variant="danger" onClick={()=>resetProgreAsync()}>Сбросить</Button>
+                    </div>
+                </section>
+            </form>
+        </>
+    );
+}
+
+export default ResetProgresForm;
